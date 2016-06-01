f58efdd7cfb6b585edc1d2ded914539fb1ad99a8
Samuel BIJOU <samuel.bijou@gmail.com>
2016-06-01 17:31:26 +0200

    Final update

--- index.js
+++ index.js
@@ -8,6 +8,33 @@ api.get('/', function(req, res, next)
 	res.send('Hello World') ;
 }) ;
 
+api.get('/contacts', function(req, res, next)
+{
+	res.send([]) ;
+}) ;
+
+api.get('/contacts/:name', function(req, res, next)
+{
+	res.send() ;
+}) ;
+
+api.post('/contacts/:name', function(req, res, next)
+{
+	if(req.params.name == 'exists')
+		return res.status(403).send() ;
+	res.send() ;
+}) ;
+
+api.put('/contacts/:name/:new', function(req, res, next)
+{
+	res.send() ;
+}) ;
+
+api.delete('/contacts/:name', function(req, res, next)
+{
+	res.send() ;
+}) ;
+
 
 /*
 console.log('server started on port 3000') ;
